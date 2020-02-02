7c5c46db1b04ca4989a3c886d3b5f100b916ca86
manukrishnamurthy <36338922+manukrishnamurthy@users.noreply.github.com>
2020-02-02 13:16:15 -0500

    Update State.java

--- State.java
+++ State.java
@@ -1,13 +1,18 @@
 import java.util.ArrayList;
 
 public class State {
-    private static String stateName;
-    private static ArrayList borderStates = new ArrayList();
-    private static ArrayList colorList = new ArrayList();
+    public static String stateName; //name of state
+    public static ArrayList borderStates = new ArrayList(); //list of bordering states
+    public static ArrayList colorList = new ArrayList(); //list of all colors
+    private static int numBorder; //number of bordering states
+    public static ArrayList<String> brokenColors = new ArrayList(); //list of colors state cannot be
+    public static int numBrokeColors = 0; //number of colors state cannot be
+    public static String assignedColor = "blank"; //assigned color of state
     public State(String name, ArrayList<String> borderArray, ArrayList<String> colors){
         stateName = name;
         borderStates = borderArray;
         colorList = colors;
+        numBorder = borderArray.size();
     }
     public static String getName(){
         return stateName;
@@ -18,4 +23,22 @@ public class State {
     public static ArrayList getColorList(){
         return colorList;
     }
+    public static int getNumborder(){
+        return numBorder;
+    }
+    public static void addBrokenColors(String color){
+        //add color to list of not possible colors
+        brokenColors.add(color);
+        numBrokeColors = brokenColors.size();
+    }
+    public static ArrayList<String> getBrokenColors(){
+        return brokenColors;
+    }
+    public static int getNumBrokeColors() {
+        return numBrokeColors;
+    }
+    public static void setAssignedColor(String color){
+        assignedColor = color;
+    }
 }
+

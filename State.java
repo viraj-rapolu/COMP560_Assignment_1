abd460dc159bbc8f7794e68d90e0a798196e163a
manukrishnamurthy <36338922+manukrishnamurthy@users.noreply.github.com>
2020-02-04 13:40:19 -0500

    Update State.java

--- State.java
+++ State.java
@@ -1,44 +1,47 @@
 import java.util.ArrayList;
 
 public class State {
-    public static String stateName; //name of state
-    public static ArrayList borderStates = new ArrayList(); //list of bordering states
-    public static ArrayList colorList = new ArrayList(); //list of all colors
-    private static int numBorder; //number of bordering states
-    public static ArrayList<String> brokenColors = new ArrayList(); //list of colors state cannot be
-    public static int numBrokeColors = 0; //number of colors state cannot be
-    public static String assignedColor = "blank"; //assigned color of state
+    private String stateName; //name of state
+    private ArrayList borderStates = new ArrayList(); //list of bordering states
+    public ArrayList colorList = new ArrayList(); //list of all colors
+    private int numBorder; //number of bordering states
+    public ArrayList<String> brokenColors = new ArrayList(); //list of colors state cannot be
+    public int numBrokeColors = 0; //number of colors state cannot be
+    public String assignedColor = "blank"; //assigned color of state
     public State(String name, ArrayList<String> borderArray, ArrayList<String> colors){
         stateName = name;
         borderStates = borderArray;
         colorList = colors;
         numBorder = borderArray.size();
     }
-    public static String getName(){
+    public String getName(){
         return stateName;
     }
-    public static ArrayList getBorderStates(){
+    public ArrayList getBorderStates(){
         return borderStates;
     }
-    public static ArrayList getColorList(){
+    public ArrayList getColorList(){
         return colorList;
     }
-    public static int getNumborder(){
+    public int getNumborder(){
         return numBorder;
     }
-    public static void addBrokenColors(String color){
+    public void addBrokenColors(String color){
         //add color to list of not possible colors
         brokenColors.add(color);
         numBrokeColors = brokenColors.size();
     }
-    public static ArrayList<String> getBrokenColors(){
+    public ArrayList<String> getBrokenColors(){
         return brokenColors;
     }
-    public static int getNumBrokeColors() {
+    public int getNumBrokeColors() {
         return numBrokeColors;
     }
-    public static void setAssignedColor(String color){
+    public void setAssignedColor(String color){
         assignedColor = color;
+    }
+    public String toString(){
+        return stateName+": "+assignedColor;
     }
 }
 
